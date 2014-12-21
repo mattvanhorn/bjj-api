59ac2dfd0039ec0c0b830b2c02ea3833d7efa41d
Matt Van Horn <mattvanhorn@gmail.com>
2014-12-21 11:23:58 -0800

    middleware?

--- app/api/hello.rb
+++ app/api/hello.rb
@@ -1,6 +1,22 @@
+class ApiSkylightInstrumenter < Grape::Middleware::Base
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    Skylight.instrument do
+      status, headers, response = @app.call(env)
+    end
+  end
+end
+
 class API::Hello < Grape::API
+  helpers Skylight::Helpers
+
+  use ApiSkylightInstrumenter
+
   desc "Hello World"
   get '/' do
-    'hello world!!'
+    { hello: "world" }
   end
 end
